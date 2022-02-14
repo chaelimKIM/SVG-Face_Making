46645a4ae1a755ff486a5b649d26bee0e0e6a204
CH.K <ellentree@naver.com>
2022-02-14 18:09:16 +0900

    update

---
+++ home.js
@@ -0,0 +1,331 @@
+function face(){
+    var f1 = document.getElementById("f1")
+    var f2 = document.getElementById("f2")
+    var f3 = document.getElementById("f3")
+    var f4 = document.getElementById("f4")
+    var e1 = document.getElementById("e1")
+    var e2 = document.getElementById("e2")
+    var e3 = document.getElementById("e3")
+    var e4 = document.getElementById("e4")
+    var n1 = document.getElementById("n1")
+    var n2 = document.getElementById("n2")
+    var n3 = document.getElementById("n3")
+    var n4 = document.getElementById("n4")
+    var m1 = document.getElementById("m1")
+    var m2 = document.getElementById("m2")
+    var m3 = document.getElementById("m3")
+    var m4 = document.getElementById("m4")
+    var fc1 = document.getElementById("fc1")
+    var fc2 = document.getElementById("fc2")
+    var fc3 = document.getElementById("fc3")
+    var fc4 = document.getElementById("fc4")
+    var ec1 = document.getElementById("ec1")
+    var ec2 = document.getElementById("ec2")
+    var ec3 = document.getElementById("ec3")
+    var ec4 = document.getElementById("ec4")
+
+    f1.style.display = "block";
+    f2.style.display = "block";
+    f3.style.display = "block";
+    f4.style.display = "block";
+    e1.style.display = "none";
+    e2.style.display = "none";
+    e3.style.display = "none";
+    e4.style.display = "none";
+    n1.style.display = "none";
+    n2.style.display = "none";
+    n3.style.display = "none";
+    n4.style.display = "none";
+    m1.style.display = "none";
+    m2.style.display = "none";
+    m3.style.display = "none";
+    m4.style.display = "none";
+    fc1.style.display = "none";
+    fc2.style.display = "none";
+    fc3.style.display = "none";
+    fc4.style.display = "none";
+    ec1.style.display = "none";
+    ec2.style.display = "none";
+    ec3.style.display = "none";
+    ec4.style.display = "none";
+}
+
+function face_color(){
+    var f1 = document.getElementById("f1")
+    var f2 = document.getElementById("f2")
+    var f3 = document.getElementById("f3")
+    var f4 = document.getElementById("f4")
+    var e1 = document.getElementById("e1")
+    var e2 = document.getElementById("e2")
+    var e3 = document.getElementById("e3")
+    var e4 = document.getElementById("e4")
+    var n1 = document.getElementById("n1")
+    var n2 = document.getElementById("n2")
+    var n3 = document.getElementById("n3")
+    var n4 = document.getElementById("n4")
+    var m1 = document.getElementById("m1")
+    var m2 = document.getElementById("m2")
+    var m3 = document.getElementById("m3")
+    var m4 = document.getElementById("m4")
+    var fc1 = document.getElementById("fc1")
+    var fc2 = document.getElementById("fc2")
+    var fc3 = document.getElementById("fc3")
+    var fc4 = document.getElementById("fc4")
+    var ec1 = document.getElementById("ec1")
+    var ec2 = document.getElementById("ec2")
+    var ec3 = document.getElementById("ec3")
+    var ec4 = document.getElementById("ec4")
+
+    f1.style.display = "none";
+    f2.style.display = "none";
+    f3.style.display = "none";
+    f4.style.display = "none";
+    e1.style.display = "none";
+    e2.style.display = "none";
+    e3.style.display = "none";
+    e4.style.display = "none";
+    n1.style.display = "none";
+    n2.style.display = "none";
+    n3.style.display = "none";
+    n4.style.display = "none";
+    m1.style.display = "none";
+    m2.style.display = "none";
+    m3.style.display = "none";
+    m4.style.display = "none";
+    fc1.style.display = "block";
+    fc2.style.display = "block";
+    fc3.style.display = "block";
+    fc4.style.display = "block";
+    ec1.style.display = "none";
+    ec2.style.display = "none";
+    ec3.style.display = "none";
+    ec4.style.display = "none";
+}
+
+function eyes(){
+    var f1 = document.getElementById("f1")
+    var f2 = document.getElementById("f2")
+    var f3 = document.getElementById("f3")
+    var f4 = document.getElementById("f4")
+    var e1 = document.getElementById("e1")
+    var e2 = document.getElementById("e2")
+    var e3 = document.getElementById("e3")
+    var e4 = document.getElementById("e4")
+    var n1 = document.getElementById("n1")
+    var n2 = document.getElementById("n2")
+    var n3 = document.getElementById("n3")
+    var n4 = document.getElementById("n4")
+    var m1 = document.getElementById("m1")
+    var m2 = document.getElementById("m2")
+    var m3 = document.getElementById("m3")
+    var m4 = document.getElementById("m4")
+    var fc1 = document.getElementById("fc1")
+    var fc2 = document.getElementById("fc2")
+    var fc3 = document.getElementById("fc3")
+    var fc4 = document.getElementById("fc4")
+    var ec1 = document.getElementById("ec1")
+    var ec2 = document.getElementById("ec2")
+    var ec3 = document.getElementById("ec3")
+    var ec4 = document.getElementById("ec4")
+
+    f1.style.display = "none";
+    f2.style.display = "none";
+    f3.style.display = "none";
+    f4.style.display = "none";
+    e1.style.display = "block";
+    e2.style.display = "block";
+    e3.style.display = "block";
+    e4.style.display = "block";
+    n1.style.display = "none";
+    n2.style.display = "none";
+    n3.style.display = "none";
+    n4.style.display = "none";
+    m1.style.display = "none";
+    m2.style.display = "none";
+    m3.style.display = "none";
+    m4.style.display = "none";
+    fc1.style.display = "none";
+    fc2.style.display = "none";
+    fc3.style.display = "none";
+    fc4.style.display = "none";
+    ec1.style.display = "none";
+    ec2.style.display = "none";
+    ec3.style.display = "none";
+    ec4.style.display = "none";
+}
+
+
+function eyes_color(){
+    var f1 = document.getElementById("f1")
+    var f2 = document.getElementById("f2")
+    var f3 = document.getElementById("f3")
+    var f4 = document.getElementById("f4")
+    var e1 = document.getElementById("e1")
+    var e2 = document.getElementById("e2")
+    var e3 = document.getElementById("e3")
+    var e4 = document.getElementById("e4")
+    var n1 = document.getElementById("n1")
+    var n2 = document.getElementById("n2")
+    var n3 = document.getElementById("n3")
+    var n4 = document.getElementById("n4")
+    var m1 = document.getElementById("m1")
+    var m2 = document.getElementById("m2")
+    var m3 = document.getElementById("m3")
+    var m4 = document.getElementById("m4")
+    var fc1 = document.getElementById("fc1")
+    var fc2 = document.getElementById("fc2")
+    var fc3 = document.getElementById("fc3")
+    var fc4 = document.getElementById("fc4")
+    var ec1 = document.getElementById("ec1")
+    var ec2 = document.getElementById("ec2")
+    var ec3 = document.getElementById("ec3")
+    var ec4 = document.getElementById("ec4")
+
+    f1.style.display = "none";
+    f2.style.display = "none";
+    f3.style.display = "none";
+    f4.style.display = "none";
+    e1.style.display = "none";
+    e2.style.display = "none";
+    e3.style.display = "none";
+    e4.style.display = "none";
+    n1.style.display = "none";
+    n2.style.display = "none";
+    n3.style.display = "none";
+    n4.style.display = "none";
+    m1.style.display = "none";
+    m2.style.display = "none";
+    m3.style.display = "none";
+    m4.style.display = "none";
+    fc1.style.display = "none";
+    fc2.style.display = "none";
+    fc3.style.display = "none";
+    fc4.style.display = "none";
+    ec1.style.display = "block";
+    ec2.style.display = "block";
+    ec3.style.display = "block";
+    ec4.style.display = "block";
+}   
+
+function nose(){
+    var f1 = document.getElementById("f1")
+    var f2 = document.getElementById("f2")
+    var f3 = document.getElementById("f3")
+    var f4 = document.getElementById("f4")
+    var e1 = document.getElementById("e1")
+    var e2 = document.getElementById("e2")
+    var e3 = document.getElementById("e3")
+    var e4 = document.getElementById("e4")
+    var n1 = document.getElementById("n1")
+    var n2 = document.getElementById("n2")
+    var n3 = document.getElementById("n3")
+    var n4 = document.getElementById("n4")
+    var m1 = document.getElementById("m1")
+    var m2 = document.getElementById("m2")
+    var m3 = document.getElementById("m3")
+    var m4 = document.getElementById("m4")
+    var fc1 = document.getElementById("fc1")
+    var fc2 = document.getElementById("fc2")
+    var fc3 = document.getElementById("fc3")
+    var fc4 = document.getElementById("fc4")
+    var ec1 = document.getElementById("ec1")
+    var ec2 = document.getElementById("ec2")
+    var ec3 = document.getElementById("ec3")
+    var ec4 = document.getElementById("ec4")
+
+    f1.style.display = "none";
+    f2.style.display = "none";
+    f3.style.display = "none";
+    f4.style.display = "none";
+    e1.style.display = "none";
+    e2.style.display = "none";
+    e3.style.display = "none";
+    e4.style.display = "none";
+    n1.style.display = "block";
+    n2.style.display = "block";
+    n3.style.display = "block";
+    n4.style.display = "block";
+    m1.style.display = "none";
+    m2.style.display = "none";
+    m3.style.display = "none";
+    m4.style.display = "none";
+    fc1.style.display = "none";
+    fc2.style.display = "none";
+    fc3.style.display = "none";
+    fc4.style.display = "none";
+    ec1.style.display = "none";
+    ec2.style.display = "none";
+    ec3.style.display = "none";
+    ec4.style.display = "none";
+}
+
+function mouth(){
+    var f1 = document.getElementById("f1")
+    var f2 = document.getElementById("f2")
+    var f3 = document.getElementById("f3")
+    var f4 = document.getElementById("f4")
+    var e1 = document.getElementById("e1")
+    var e2 = document.getElementById("e2")
+    var e3 = document.getElementById("e3")
+    var e4 = document.getElementById("e4")
+    var n1 = document.getElementById("n1")
+    var n2 = document.getElementById("n2")
+    var n3 = document.getElementById("n3")
+    var n4 = document.getElementById("n4")
+    var m1 = document.getElementById("m1")
+    var m2 = document.getElementById("m2")
+    var m3 = document.getElementById("m3")
+    var m4 = document.getElementById("m4")
+    var fc1 = document.getElementById("fc1")
+    var fc2 = document.getElementById("fc2")
+    var fc3 = document.getElementById("fc3")
+    var fc4 = document.getElementById("fc4")
+    var ec1 = document.getElementById("ec1")
+    var ec2 = document.getElementById("ec2")
+    var ec3 = document.getElementById("ec3")
+    var ec4 = document.getElementById("ec4")
+
+    f1.style.display = "none";
+    f2.style.display = "none";
+    f3.style.display = "none";
+    f4.style.display = "none";
+    e1.style.display = "none";
+    e2.style.display = "none";
+    e3.style.display = "none";
+    e4.style.display = "none";
+    n1.style.display = "none";
+    n2.style.display = "none";
+    n3.style.display = "none";
+    n4.style.display = "none";
+    m1.style.display = "block";
+    m2.style.display = "block";
+    m3.style.display = "block";
+    m4.style.display = "block";
+    fc1.style.display = "none";
+    fc2.style.display = "none";
+    fc3.style.display = "none";
+    fc4.style.display = "none";
+    ec1.style.display = "none";
+    ec2.style.display = "none";
+    ec3.style.display = "none";
+    ec4.style.display = "none";
+}
+
+function d1(){
+    alert("hi");
+    var a = document.getElementById("pic");
+    a.innerHTML = "<svg id=\"f1\" viewBox=\"0 0 300 300\"><circle cx=\"150\" cy=\"150\" r=\"80\" style=\"stroke: #000; stroke-width: 5; fill: transparent;\" ></circle></svg>";
+
+}
+
+function d2(){
+    
+}
+
+function d3(){
+    
+}
+
+function d4(){
+    
+}
