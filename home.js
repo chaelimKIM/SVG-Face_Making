b8b4d97a48fdaa66fa3540c760ceb2de644845e6
CH.K <ellentree@naver.com>
2022-02-16 10:02:55 +0900

    update

--- home.js
+++ home.js
@@ -1,4 +1,5 @@
 function face(){
+    var side = document.getElementById("sidebar");
     var f1 = document.getElementById("f1")
     var f2 = document.getElementById("f2")
     var f3 = document.getElementById("f3")
@@ -24,6 +25,7 @@ function face(){
     var ec3 = document.getElementById("ec3")
     var ec4 = document.getElementById("ec4")
 
+    side.value = "face";
     f1.style.display = "block";
     f2.style.display = "block";
     f3.style.display = "block";
@@ -51,6 +53,7 @@ function face(){
 }
 
 function face_color(){
+    var side = document.getElementById("sidebar");
     var f1 = document.getElementById("f1")
     var f2 = document.getElementById("f2")
     var f3 = document.getElementById("f3")
@@ -76,6 +79,7 @@ function face_color(){
     var ec3 = document.getElementById("ec3")
     var ec4 = document.getElementById("ec4")
 
+    side.value = "face_color";
     f1.style.display = "none";
     f2.style.display = "none";
     f3.style.display = "none";
@@ -103,6 +107,7 @@ function face_color(){
 }
 
 function eyes(){
+    var side = document.getElementById("sidebar");
     var f1 = document.getElementById("f1")
     var f2 = document.getElementById("f2")
     var f3 = document.getElementById("f3")
@@ -128,6 +133,7 @@ function eyes(){
     var ec3 = document.getElementById("ec3")
     var ec4 = document.getElementById("ec4")
 
+    side.value = "eyes";
     f1.style.display = "none";
     f2.style.display = "none";
     f3.style.display = "none";
@@ -156,6 +162,7 @@ function eyes(){
 
 
 function eyes_color(){
+    var side = document.getElementById("sidebar");
     var f1 = document.getElementById("f1")
     var f2 = document.getElementById("f2")
     var f3 = document.getElementById("f3")
@@ -181,6 +188,7 @@ function eyes_color(){
     var ec3 = document.getElementById("ec3")
     var ec4 = document.getElementById("ec4")
 
+    side.value = "eyes_color";
     f1.style.display = "none";
     f2.style.display = "none";
     f3.style.display = "none";
@@ -208,6 +216,7 @@ function eyes_color(){
 }   
 
 function nose(){
+    var side = document.getElementById("sidebar");
     var f1 = document.getElementById("f1")
     var f2 = document.getElementById("f2")
     var f3 = document.getElementById("f3")
@@ -233,6 +242,7 @@ function nose(){
     var ec3 = document.getElementById("ec3")
     var ec4 = document.getElementById("ec4")
 
+    side.value = "nose";
     f1.style.display = "none";
     f2.style.display = "none";
     f3.style.display = "none";
@@ -260,6 +270,7 @@ function nose(){
 }
 
 function mouth(){
+    var side = document.getElementById("sidebar");
     var f1 = document.getElementById("f1")
     var f2 = document.getElementById("f2")
     var f3 = document.getElementById("f3")
@@ -285,6 +296,7 @@ function mouth(){
     var ec3 = document.getElementById("ec3")
     var ec4 = document.getElementById("ec4")
 
+    side.value = "mouth";
     f1.style.display = "none";
     f2.style.display = "none";
     f3.style.display = "none";
@@ -312,20 +324,266 @@ function mouth(){
 }
 
 function d1(){
-    alert("hi");
-    var a = document.getElementById("pic");
-    a.innerHTML = "<svg id=\"f1\" viewBox=\"0 0 300 300\"><circle cx=\"150\" cy=\"150\" r=\"80\" style=\"stroke: #000; stroke-width: 5; fill: transparent;\" ></circle></svg>";
+    var side = document.getElementById("sidebar");
+    
+    var f_1 = document.getElementById("f_1")
+    var e_1 = document.getElementById("e_1")
+    var n_1 = document.getElementById("n_1")
+    var m_1 = document.getElementById("m_1")
+    var f_2 = document.getElementById("f_2")
+    var e_2 = document.getElementById("e_2")
+    var n_2 = document.getElementById("n_2")
+    var m_2 = document.getElementById("m_2")
+    var f_3 = document.getElementById("f_3")
+    var e_3 = document.getElementById("e_3")
+    var n_3 = document.getElementById("n_3")
+    var m_3 = document.getElementById("m_3")
+    var f_4 = document.getElementById("f_4")
+    var e_4 = document.getElementById("e_4")
+    var n_4 = document.getElementById("n_4")
+    var m_4 = document.getElementById("m_4")
+
+    if(side.value == "face"){
+        f_1.style.display = "block";
+        e_1.style.display = "none";
+        n_1.style.display = "none";
+        m_1.style.display = "none";
+        f_2.style.display = "none";
+        f_3.style.display = "none";
+        f_4.style.display = "none";
+    }
+    else if(side.value == "face_color"){
+    }
+    else if(side.value == "eyes"){
+        f_1.style.display = "none";
+        e_1.style.display = "block";
+        n_1.style.display = "none";
+        m_1.style.display = "none";
+        e_2.style.display = "none";
+        e_3.style.display = "none";
+        e_4.style.display = "none";
+
+    }
+    else if(side.value == "eyes_color"){
+
+    }
+    else if(side.value == "nose"){
+        f_1.style.display = "none";
+        e_1.style.display = "none";
+        n_1.style.display = "block";
+        m_1.style.display = "none";
+        n_2.style.display = "none";
+        n_3.style.display = "none";
+        n_4.style.display = "none";
+    }
+    else if(side.value == "mouth"){
+        f_1.style.display = "none";
+        e_1.style.display = "none";
+        n_1.style.display = "none";
+        m_1.style.display = "block";
+        m_2.style.display = "none";
+        m_3.style.display = "none";
+        m_4.style.display = "none";
+    }
+    else{
+        side.value = "none";
+    }
 
 }
 
 function d2(){
+    var side = document.getElementById("sidebar");
     
+    var f_1 = document.getElementById("f_1")
+    var e_1 = document.getElementById("e_1")
+    var n_1 = document.getElementById("n_1")
+    var m_1 = document.getElementById("m_1")
+    var f_2 = document.getElementById("f_2")
+    var e_2 = document.getElementById("e_2")
+    var n_2 = document.getElementById("n_2")
+    var m_2 = document.getElementById("m_2")
+    var f_3 = document.getElementById("f_3")
+    var e_3 = document.getElementById("e_3")
+    var n_3 = document.getElementById("n_3")
+    var m_3 = document.getElementById("m_3")
+    var f_4 = document.getElementById("f_4")
+    var e_4 = document.getElementById("e_4")
+    var n_4 = document.getElementById("n_4")
+    var m_4 = document.getElementById("m_4")
+
+    f_1.style.display = "none";
+    f_2.style.display = "none";
+    f_3.style.display = "none";
+    f_4.style.display = "none";
+    e_1.style.display = "none";
+    e_2.style.display = "none";
+    e_3.style.display = "none";
+    e_4.style.display = "none";
+    n_1.style.display = "none";
+    n_2.style.display = "none";
+    n_3.style.display = "none";
+    n_4.style.display = "none";
+    m_1.style.display = "none";
+    m_2.style.display = "none";
+    m_3.style.display = "none";
+    m_4.style.display = "none";
+
+    if(side.value == "face"){
+        f_2.style.display = "block";
+    }
+    else if(side.value == "face_color"){
+    }
+    else if(side.value == "eyes"){
+        e_2.style.display = "block";
+    }
+    else if(side.value == "eyes_color"){
+
+    }
+    else if(side.value == "nose"){
+        n_2.style.display = "block";
+
+    }
+    else if(side.value == "mouth"){
+        m_2.style.display = "block";
+    }
+    else{
+        side.value = "none";
+    }
 }
 
 function d3(){
+    var side = document.getElementById("sidebar");
+    
+    var f_1 = document.getElementById("f_1")
+    var e_1 = document.getElementById("e_1")
+    var n_1 = document.getElementById("n_1")
+    var m_1 = document.getElementById("m_1")
+    var f_2 = document.getElementById("f_2")
+    var e_2 = document.getElementById("e_2")
+    var n_2 = document.getElementById("n_2")
+    var m_2 = document.getElementById("m_2")
+    var f_3 = document.getElementById("f_3")
+    var e_3 = document.getElementById("e_3")
+    var n_3 = document.getElementById("n_3")
+    var m_3 = document.getElementById("m_3")
+    var f_4 = document.getElementById("f_4")
+    var e_4 = document.getElementById("e_4")
+    var n_4 = document.getElementById("n_4")
+    var m_4 = document.getElementById("m_4")
+
+    if(side.value == "face"){
+        f_3.style.display = "block";
+        e_3.style.display = "none";
+        n_3.style.display = "none";
+        m_3.style.display = "none";
+        f_1.style.display = "none";
+        f_2.style.display = "none";
+        f_4.style.display = "none";
+    }
+    else if(side.value == "face_color"){
+    }
+    else if(side.value == "eyes"){
+        f_3.style.display = "none";
+        e_3.style.display = "block";
+        n_3.style.display = "none";
+        m_3.style.display = "none";
+        e_1.style.display = "none";
+        e_2.style.display = "none";
+        e_4.style.display = "none";
+    }
+    else if(side.value == "eyes_color"){
+
+    }
+    else if(side.value == "nose"){
+        f_3.style.display = "none";
+        e_3.style.display = "none";
+        n_3.style.display = "block";
+        m_3.style.display = "none";
+        n_1.style.display = "none";
+        n_2.style.display = "none";
+        n_4.style.display = "none";
+
+    }
+    else if(side.value == "mouth"){
+        f_3.style.display = "none";
+        e_3.style.display = "none";
+        n_3.style.display = "none";
+        m_3.style.display = "block";
+        m_1.style.display = "none";
+        m_2.style.display = "none";
+        m_4.style.display = "none";
+    }
+    else{
+        side.value = "none";
+    }
     
 }
 
 function d4(){
+    var side = document.getElementById("sidebar");
+    
+    var f_1 = document.getElementById("f_1")
+    var e_1 = document.getElementById("e_1")
+    var n_1 = document.getElementById("n_1")
+    var m_1 = document.getElementById("m_1")
+    var f_2 = document.getElementById("f_2")
+    var e_2 = document.getElementById("e_2")
+    var n_2 = document.getElementById("n_2")
+    var m_2 = document.getElementById("m_2")
+    var f_3 = document.getElementById("f_3")
+    var e_3 = document.getElementById("e_3")
+    var n_3 = document.getElementById("n_3")
+    var m_3 = document.getElementById("m_3")
+    var f_4 = document.getElementById("f_4")
+    var e_4 = document.getElementById("e_4")
+    var n_4 = document.getElementById("n_4")
+    var m_4 = document.getElementById("m_4")
+
+    if(side.value == "face"){
+        f_4.style.display = "block";
+        e_4.style.display = "none";
+        n_4.style.display = "none";
+        m_4.style.display = "none";
+        f_1.style.display = "none";
+        f_2.style.display = "none";
+        f_3.style.display = "none";
+    }
+    else if(side.value == "face_color"){
+    }
+    else if(side.value == "eyes"){
+        f_4.style.display = "none";
+        e_4.style.display = "block";
+        n_4.style.display = "none";
+        m_4.style.display = "none";
+        e_1.style.display = "none";
+        e_2.style.display = "none";
+        e_3.style.display = "none";
+
+    }
+    else if(side.value == "eyes_color"){
+
+    }
+    else if(side.value == "nose"){
+        f_4.style.display = "none";
+        e_4.style.display = "none";
+        n_4.style.display = "block";
+        m_4.style.display = "none";
+        n_1.style.display = "none";
+        n_2.style.display = "none";
+        n_3.style.display = "none";
+
+    }
+    else if(side.value == "mouth"){
+        f_4.style.display = "none";
+        e_4.style.display = "none";
+        n_4.style.display = "none";
+        m_4.style.display = "block";
+        m_1.style.display = "none";
+        m_2.style.display = "none";
+        m_3.style.display = "none";
+    }
+    else{
+        side.value = "none";
+    }
     
 }
